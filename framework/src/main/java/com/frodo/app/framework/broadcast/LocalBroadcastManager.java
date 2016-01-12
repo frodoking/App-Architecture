4769cc5d4ef5a42ed94e75d40afd1af41ca1b368
frodo <awangyun8@gmail.com>
2016-01-12 15:26:12 +0800

    fix error

--- framework/src/main/java/com/frodo/app/framework/broadcast/LocalBroadcastManager.java
+++ framework/src/main/java/com/frodo/app/framework/broadcast/LocalBroadcastManager.java
@@ -1,7 +1,7 @@
 package com.frodo.app.framework.broadcast;
 
 /**
- * Created by frodo on 2015/12/29. 消息中心管理(一定是轻量级的，多模块非必需交互建议采用MainController中转方式)
+ * Created by frodo on 2015/12/29.  message center , to manage communicate between with subsystem
  */
 public interface LocalBroadcastManager {
     void send(Object key);
