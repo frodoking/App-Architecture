53f3355c748e37dd4177a2ef104abfd5a244d424
frodo <awangyun8@gmail.com>
2016-03-11 17:36:30 +0800

    fix ThreadPoolExecutor: java.util.concurrent.FutureTask cannot be cast to java.lang.Comparable

--- app/src/main/java/com/frodo/app/android/core/task/AndroidExecutor.java
+++ app/src/main/java/com/frodo/app/android/core/task/AndroidExecutor.java
@@ -2,7 +2,10 @@ package com.frodo.app.android.core.task;
 
 import com.frodo.app.framework.log.Logger;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
 import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.RunnableFuture;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
@@ -58,6 +61,49 @@ public class AndroidExecutor extends ThreadPoolExecutor {
     private AndroidExecutor(int corePoolSize, ThreadFactory threadFactory) {
         super(corePoolSize, corePoolSize, 0, TimeUnit.MILLISECONDS,
                 new PriorityBlockingQueue<Runnable>(), threadFactory);
+    }
+
+    @Override
+    protected <T> RunnableFuture<T> newTaskFor(Runnable runnable, T value) {
+        return new ComparableFutureTask<>(runnable, value);
+    }
+
+    @Override
+    protected <T> RunnableFuture<T> newTaskFor(Callable<T> callable) {
+        return new ComparableFutureTask<>(callable);
+    }
+
+    protected class ComparableFutureTask<V> extends FutureTask<V> implements Comparable<ComparableFutureTask<V>> {
+        private Object object;
+
+        public ComparableFutureTask(Callable<V> callable) {
+            super(callable);
+            object = callable;
+        }
+
+        public ComparableFutureTask(Runnable runnable, V result) {
+            super(runnable, result);
+            object = runnable;
+        }
+
+        @Override
+        @SuppressWarnings("unchecked")
+        public int compareTo(ComparableFutureTask<V> o) {
+            if (this == o) {
+                return 0;
+            }
+            if (o == null) {
+                return -1; // high priority
+            }
+            if (object != null && o.object != null) {
+                if (object.getClass().equals(o.object.getClass())) {
+                    if (object instanceof Comparable) {
+                        return ((Comparable) object).compareTo(o.object);
+                    }
+                }
+            }
+            return 0;
+        }
     }
 
     /**
