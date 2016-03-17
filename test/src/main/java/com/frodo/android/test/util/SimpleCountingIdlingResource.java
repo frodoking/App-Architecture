84129c53178681d7519f5a28ca6f4acf9389bf7f
Zeeshan Asghar <zeeshan.asghar@devfactory.com>
2016-03-17 07:03:29 +0500

    squid:S1066 Collapsible 'if' statements should be merged

--- test/src/main/java/com/frodo/android/test/util/SimpleCountingIdlingResource.java
+++ test/src/main/java/com/frodo/android/test/util/SimpleCountingIdlingResource.java
@@ -80,11 +80,8 @@ public final class SimpleCountingIdlingResource implements IdlingResource {
      */
     public void decrement() {
         int counterVal = counter.decrementAndGet();
-        if (counterVal == 0) {
-            // we've gone from non-zero to zero. That means we're idle now! Tell espresso.
-            if (null != resourceCallback) {
-                resourceCallback.onTransitionToIdle();
-            }
+        if (counterVal == 0 && null != resourceCallback) {// we've gone from non-zero to zero. That means we're idle now! Tell espresso.
+            resourceCallback.onTransitionToIdle();
         }
 
         if (counterVal < 0) {
