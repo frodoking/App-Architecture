e1c7575d2ffe63a4a1ce931e5112febf015e49f5
frodoking <awangyun8@gmail.com>
2016-09-12 17:06:14 +0800

    fix onActivityResult

--- app/src/main/java/com/frodo/app/android/ui/activity/RedirectActivity.java
+++ app/src/main/java/com/frodo/app/android/ui/activity/RedirectActivity.java
@@ -2,8 +2,11 @@ package com.frodo.app.android.ui.activity;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.support.v4.app.Fragment;
 
 import com.frodo.app.android.ui.FragmentScheduler;
+
+import java.util.List;
 
 /**
  * Created by frodo on 2016/5/26.
@@ -27,5 +30,19 @@ public class RedirectActivity extends FragmentContainerActivity {
 
     @Override
     public void initBusiness() {
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (getIntent().getBooleanExtra("needActivityResult", false)) {
+            List<Fragment> fragments = getSupportFragmentManager().getFragments();
+            for (Fragment f : fragments) {
+                if (f != null) {
+                    f.onActivityResult(requestCode, resultCode, data);
+                }
+            }
+        }
     }
 }
